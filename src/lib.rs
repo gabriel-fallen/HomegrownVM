e8329b708f768e7546a1c8f8a4a408be2b20484f
Alexander Tchitchigin <sad.ronin@gmail.com>
2019-01-11 22:39:19 +0300

    Ready to run.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@ mod vm;
 #[cfg(test)]
 mod tests {
     use crate::vm::*;
-    
+
     #[test]
     fn it_works() {
         let code = [Instruction::Push(1)];
-        let mut vm = VM_state::new(&code);
+        let mut vm = VmState::new(&code);
     }
 }
--- src/vm.rs
+++ src/vm.rs
@@ -9,19 +9,46 @@ pub enum Instruction {
   Div
 }
 
-pub trait VM {
-  fn run(&mut self);
-  fn get(&self, index: i32) -> i64; // get i-th element from the stack, negative index counts from the bottom
+pub enum VmError {
+  OutOfBounds,
+  OutOfMemory
 }
 
-pub struct VM_state<'a> {
+pub trait VM {
+  fn run(&mut self);
+  fn get(&self, index: i32) -> Result<i64, VmError>; // get i-th element from the stack, negative index counts from the bottom
+}
+
+pub struct VmState<'a> {
   pc: u32,
   code: &'a [Instruction],
   stack: Vec<i64>
 }
 
-impl<'a> VM_state<'a> {
-  pub fn new(code: &'a [Instruction]) -> VM_state<'a> {
-    VM_state {pc: 0, code, stack: vec![]}
+impl<'a> VmState<'a> {
+  pub fn new(code: &'a [Instruction]) -> VmState<'a> {
+    VmState {pc: 0, code, stack: vec![]}
+  }
+}
+
+impl<'a> VM for VmState<'a> {
+  fn get(&self, index: i32) -> Result<i64, VmError> {
+    if index < 0 {
+      let idx = self.stack.len() as i32 + index;
+      if idx < 0 {
+        return Err(VmError::OutOfBounds)
+      }
+      return Ok(self.stack[idx as usize])
+    }
+
+    let idx = index as usize;
+    if idx > self.stack.len() - 1 {
+      return Err(VmError::OutOfBounds)
+    }
+    Ok(self.stack[idx])
+  }
+
+  fn run(&mut self) {
+    //
   }
 }
