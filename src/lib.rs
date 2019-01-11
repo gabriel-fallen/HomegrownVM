abb648cb9a1e5a735ef2703379916e09c92a8b6c
Alexander Tchitchigin <sad.ronin@gmail.com>
2019-01-11 23:49:48 +0300

    Arithmetic.

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,23 @@ mod vm;
 
 #[cfg(test)]
 mod tests {
-    use crate::vm::*;
+    use crate::vm::{*, Instruction::*};
 
     #[test]
-    fn it_works() {
-        let code = [Instruction::Push(1)];
+    fn vm_works() {
+        let code = [Push(1)];
         let mut vm = VmState::new(&code);
+        let res = vm.run();
+        assert_eq!(res, Ok(()));
+        assert_eq!(vm.get(0), Ok(1));
+    }
+
+    #[test]
+    fn vm_adds() {
+        let code = [Push(1), Push(4), Add];
+        let mut vm = VmState::new(&code);
+        let res = vm.run();
+        assert_eq!(res, Ok(()));
+        assert_eq!(vm.get(0), Ok(5));
     }
 }
--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,6 @@
 // VM instructions, state and interpreter
 
+#[derive(Debug)]
 pub enum Instruction {
   Push(i64),
   Pop,
@@ -9,18 +10,20 @@ pub enum Instruction {
   Div
 }
 
+#[derive(Debug, PartialEq)]
 pub enum VmError {
   OutOfBounds,
-  OutOfMemory
+  OutOfMemory,
+  DivByZero
 }
 
 pub trait VM {
-  fn run(&mut self);
+  fn run(&mut self) -> Result<(), VmError>;
   fn get(&self, index: i32) -> Result<i64, VmError>; // get i-th element from the stack, negative index counts from the bottom
 }
 
 pub struct VmState<'a> {
-  pc: u32,
+  pc: usize,
   code: &'a [Instruction],
   stack: Vec<i64>
 }
@@ -48,7 +51,66 @@ impl<'a> VM for VmState<'a> {
     Ok(self.stack[idx])
   }
 
-  fn run(&mut self) {
-    //
+  fn run(&mut self) -> Result<(), VmError> {
+    use self::Instruction::*;
+
+    while self.pc < self.code.len() {
+      match self.code[self.pc] {
+        Push(val) => self.stack.push(val),
+        Pop => drop(self.stack.pop()),
+        Add => {
+          let res = add(self.stack.pop(), self.stack.pop())?;
+          self.stack.push(res)
+        },
+        Sub => {
+          let res = sub(self.stack.pop(), self.stack.pop())?;
+          self.stack.push(res)
+        },
+        Mul => {
+          let res = mul(self.stack.pop(), self.stack.pop())?;
+          self.stack.push(res)
+        },
+        Div => {
+          let res = div(self.stack.pop(), self.stack.pop())?;
+          self.stack.push(res)
+        },
+      }
+      self.pc += 1;
+    }
+
+    Ok(())
+  }
+}
+
+fn add(val1: Option<i64>, val2: Option<i64>) -> Result<i64, VmError> {
+  match (val1, val2) {
+    (Some(a), Some(b)) => Ok(a + b),
+    _ => Err(VmError::OutOfBounds)
+  }
+}
+
+fn sub(val1: Option<i64>, val2: Option<i64>) -> Result<i64, VmError> {
+  match (val1, val2) {
+    (Some(a), Some(b)) => Ok(a - b),
+    _ => Err(VmError::OutOfBounds)
+  }
+}
+
+fn mul(val1: Option<i64>, val2: Option<i64>) -> Result<i64, VmError> {
+  match (val1, val2) {
+    (Some(a), Some(b)) => Ok(a * b),
+    _ => Err(VmError::OutOfBounds)
+  }
+}
+
+fn div(val1: Option<i64>, val2: Option<i64>) -> Result<i64, VmError> {
+  match (val1, val2) {
+    (Some(a), Some(b)) => {
+      if b == 0 {
+        return Err(VmError::DivByZero)
+      }
+      Ok(a / b)
+    },
+    _ => Err(VmError::OutOfBounds)
   }
 }
